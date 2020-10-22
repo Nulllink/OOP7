0028066df8478e3a25aff48c83e56ec3cc829cdd
Nulllink <alexanveld@gmail.com>
2020-10-22 21:05:23 +0300

    Comments Model

--- OO7/Model.cs
+++ OO7/Model.cs
@@ -8,14 +8,20 @@ namespace OO7
     /// </summary>
     class Student
     {
-        public int indexRB;
-        public string nameS;
-        public int indexG;
-        public string sex;
+        public int indexRB; //Индекс студенческого
+        public string nameS; //Фамилия студента
+        public int indexG; // Индекс группы
+        public string sex; // Пол студента
+        /// <summary>
+        /// Добавление студента
+        /// </summary>
         public static void Create()
         {
             Application.Run(new DataForm(1,-1));
         }
+        /// <summary>
+        /// Редактирование студента по индексу
+        /// </summary>
         public static void change(int index)
         {
             Application.Run(new DataForm(1, index));
@@ -28,10 +34,16 @@ namespace OO7
     {
         public int keyG;
         public string nameG;
+        /// <summary>
+        /// Создание новой группы
+        /// </summary>
         public static void Create()
         {
             Application.Run(new DataForm(2, -1));
         }
+        /// <summary>
+        /// Редактирование группы по индексу 
+        /// </summary>
         public static void change(int index)
         {
             Application.Run(new DataForm(2, index));
